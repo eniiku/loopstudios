16e47bf27a2722ffab2fb4aee2de73cca218e756
eniku <enikuauth@gmail.com>
2022-11-01 21:37:06 +0100

    feat(Navbar): create html chops for mobile menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,39 @@
+import { Fragment } from 'react';
+
 import logoIcon from '../../assets/images/logo.svg';
 import menuIcon from '../../assets/images/icon-hamburger.svg';
+import closeIcon from '../../assets/images/icon-close.svg';
+
+const navLinks = ['about', 'careers', 'events', 'products', 'support'];
+
+export const Menu = () => {
+  return (
+    <div>
+      <div>
+        <img src={logoIcon} alt="loopstudio's Logo Icon" />
+        <button role='navigation'>{closeIcon}</button>
+      </div>
+      <ul>
+        {navLinks.map((link) => (
+          <li key={link}>{link}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const Navbar = () => {
   return (
-    <nav>
-      <img src={logoIcon} alt='loopstudios Logo Icon' />
-      <button role='navigation'>
-        <img src={menuIcon} role='presentation' />
-      </button>
-    </nav>
+    <Fragment>
+      <nav>
+        <img src={logoIcon} alt="loopstudio's Logo Icon" />
+
+        <button role='navigation'>
+          <img src={menuIcon} role='presentation' />
+        </button>
+      </nav>
+      <Menu />
+    </Fragment>
   );
 };
 
