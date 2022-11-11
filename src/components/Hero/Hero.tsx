0fd54e4e28f60095dce0766503ae4643cd838fc8
eniiku <enikuauth@gmail.com>
2022-11-11 21:04:31 +0100

    fix: add heading text to hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,11 +1,15 @@
-import { Fragment } from 'react';
-import Navbar from '../Navbar/Navbar';
+import { Fragment } from "react";
+
+import Navbar from "../Navbar/Navbar";
 
 const Hero = () => {
   return (
-    <Fragment>
+    <div>
       <Navbar />
-    </Fragment>
+      <div>
+        <h1>immersive experiences that deliver</h1>
+      </div>
+    </div>
   );
 };
 
