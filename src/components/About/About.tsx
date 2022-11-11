d2578c1ac60b4daab8e48afad86e8105047c82e5
eniiku <enikuauth@gmail.com>
2022-11-11 21:58:53 +0100

    style(About): style about section for mobile layout

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,15 +1,19 @@
-import aboutImage from '../../assets/images/mobile/image-interactive.jpg';
+import aboutImage from "../../assets/images/mobile/image-interactive.jpg";
+import "./About.scss";
 
 const About = () => {
   return (
-    <section>
+    <section className="about app--container">
       <img
         src={aboutImage}
-        alt='User interacting with virtual reality project'
+        alt="User interacting with virtual reality project"
+        className="about__img"
       />
-      <div>
-        <h1>the leader in interactive vr</h1>
-        <p>
+      <div className="about__text">
+        <h1 className="about__text-head heading-text">
+          the leader in interactive vr
+        </h1>
+        <p className="about__text-body paragraph-text">
           Founded in 2011, Loopstudios has been producing world-class virtual
           reality projects for some of the best companies around the globe. Our
           award-winning creations have transformed businesses to through digital
